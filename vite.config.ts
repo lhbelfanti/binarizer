fddc8f595a7b5d450f79d8fa06d97ef4870e7d5b
Lucas Belfanti <lhbelfanti@gmail.com>
2025-01-05 20:08:10 -0300

    🛠️ Updated to Remix v3 and React Routes v7

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,18 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [
     remix({
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
+        v3_lazyRouteDiscovery: true,
+        v3_singleFetch: true,
+        v3_routeConfig: true,
       },
     }),
     tsconfigPaths(),
