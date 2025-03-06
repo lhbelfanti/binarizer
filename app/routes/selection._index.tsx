b2b8dac3873964695bb6cb5debf1c0e16b333651
Lucas Belfanti <lhbelfanti@gmail.com>
2025-03-06 03:59:19 -0300

    ⚡️ Implemented selection page (UI only)

--- app/routes/selection._index.tsx
+++ app/routes/selection._index.tsx
@@ -1,4 +1,9 @@
+import { Trans } from 'react-i18next';
+
 import { ActionFunction, ActionFunctionArgs, LoaderFunction, LoaderFunctionArgs, redirect } from '@remix-run/node';
+
+import CriteriaSelector from 'app/components/CriteriaSelector';
+import searchCriteria from 'app/data/search_criteria_examples.json';
 
 import { getDataFromSession } from '@services/api/session.server';
 import { SessionData } from '@services/api/types.server';
@@ -24,8 +29,21 @@ export const action: ActionFunction = async ({ request }: ActionFunctionArgs) =>
 
 const SelectionPage = () => {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="transform translate-y-[-20%]"></div>
+    <div className="flex items-center justify-center mt-16">
+      <div className="flex flex-col">
+        <div className="flex flex-col items-center gap-4 mt-4">
+          <div className="flex flex-col">
+            <h2 className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-purple-500">
+              <Trans i18nKey="selection_title" />
+            </h2>
+            <p className="text-lg text-gray-300 mt-1 font-thin opacity-80">
+              <Trans i18nKey="selection_description" />
+            </p>
+            <div className="mt-8" />
+          </div>
+          <CriteriaSelector criteria={searchCriteria} />
+        </div>
+      </div>
     </div>
   );
 };
