2a686c1c69c628b381b48f073c64124446360455
Lucas Belfanti <lhbelfanti@gmail.com>
2025-03-16 20:06:56 -0300

    ⚡️ Added dayjs to format the date and avoid SSR warning

--- app/components/TweetCard/TweetHeader/DateDisplay/index.tsx
+++ app/components/TweetCard/TweetHeader/DateDisplay/index.tsx
@@ -1,28 +1,15 @@
 import { useMemo } from 'react';
-
-import { useTranslation } from 'react-i18next';
+import dayjs from "dayjs";
 
 import { DateDisplayProps } from './types';
 
 const DateDisplay = (props: DateDisplayProps) => {
   const { postedAt } = props;
 
-  const { i18n } = useTranslation();
-
   const formattedDate = useMemo(() => {
-    const currentLanguage = i18n.language;
-
-    const parsedDate = new Date(postedAt);
-    return parsedDate.toLocaleString(currentLanguage, {
-      year: 'numeric',
-      month: 'short',
-      day: '2-digit',
-      hour: '2-digit',
-      minute: '2-digit',
-      second: '2-digit',
-      hour12: true,
-    });
-  }, [postedAt, i18n.language]);
+    const parsedDate = dayjs(postedAt);
+    return parsedDate.format("MMM DD, YYYY, hh:mm:ss A");
+  }, [postedAt]);
 
   return <div className="text-gray-400 text-xs mt-1">{formattedDate}</div>;
 };
